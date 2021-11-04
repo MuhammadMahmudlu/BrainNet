e406ef1ee32733851c051cfda3a96d761c81a91b
Muhammad Elshan <muhammadmahmudlu@gmail.com>
2021-11-04 11:49:34 +0400

    Js Lesson 1

---
+++ lesson11/script.js
@@ -0,0 +1,17 @@
+// var $traning = "TRANİNG CENTER";
+// document.write($traning);
+
+// const $traning= "MATRİX ";
+// document.write($traning);
+
+
+// let ad ="Leyla";
+// document.write(ad );
+
+
+var marka = prompt("Markasi");
+var model = prompt("modeli");
+var il = prompt (2005) ;
+var depo = prompt (10.5);
+
+document.write("Maşınımızın markası" + " " + marka + "modeli isə " + " " + model + " ili isə " + il + " "+ " benzin tutumu ise"+ " " + depo  )
